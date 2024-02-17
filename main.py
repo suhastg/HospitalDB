fe34d7bebe0f515dcdbc398d67c3e167cc09d8cc
suhastg <suhasgrt2003@gmail.com>
2024-02-18 02:43:22 +0530

    con_db

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,26 +7,32 @@ DB_URL = "mysql://root@localhost:3306/hospitaldb"
 import pymysql.cursors
 import pymysql
 
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             db='hospitaldb',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+def get_db_con():
+    # Connect to the database
+    connection = pymysql.connect(host='localhost',
+                user='root',
+                db='hospitaldb',
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor)
+    return connection
 
-try:
-    with connection.cursor() as cursor:
-        # Create a new record
-        query = "SELECT * FROM patient"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        print(f"{result=}")
-finally:
-    connection.close()
 
 @app.route("/")
-def hello_world():
-    return (
-    """<p>
-    <input type = "text" />
-    </p>""")
+def index():
+
+    connection = get_db_con()
+    result = []
+
+    data = {}
+
+    try: 
+        with connection.cursor() as cursor:
+            # Create a new record
+            query = "SELECT * FROM patient"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            data["patients"] = result
+    finally:
+        connection.close()
+
+    return render_template("index.html", data1=data)
