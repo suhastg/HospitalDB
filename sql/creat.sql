a34ec8bd9c182f96652375e72e7354f4cdefb8fb
suhastg <suhasgrt2003@gmail.com>
2024-02-17 22:46:58 +0530

    create n insert

--- sql/creat.sql
+++ sql/creat.sql
@@ -6,3 +6,57 @@ address varchar(35),
 gender varchar(10) 
 );
 
+CREATE TABLE doctor
+(
+    name varchar(30),
+    doctor_id int primary key,
+    dept varchar(20)
+);
+
+CREATE TABLE lab_report
+(
+    p_id int,
+    lab_no int primary key,
+    amount int,
+    doctor_id int,
+    report_date date,
+    foreign key (p_id) references Patient(p_id) on delete cascade
+);
+
+CREATE TABLE inpatient
+(
+    room_no int ,
+    lab_no int,
+    p_id int primary key,
+    date_of_admit date,
+    date_of_discharge date,
+    foreign key (lab_no) references lab_report(lab_no) on delete cascade,
+    foreign key (p_id) references Patient(p_id) on delete cascade
+);
+
+CREATE TABLE outpatient
+(
+    lab_no int,
+    p_id int primary key,
+    datee date,
+    foreign key (p_id) references Patient(p_id) on delete cascade,
+    foreign key (lab_no) references lab_report(lab_no) on delete cascade
+);
+
+CREATE TABLE rooms
+(
+    room_no int primary key,
+    room_type varchar (10),
+    status  varchar(20)
+);
+
+CREATE TABLE bills
+(
+    bill_no varchar(30) primary key,
+    p_id int,
+    no_of_days int,
+    medicine_charges int,
+    room_no int,
+    foreign key (p_id) references Patient(p_id) on delete cascade,
+    foreign key (room_no) references rooms (room_no) on delete cascade
+);
---
+++ sql/insert.sql
@@ -0,0 +1,38 @@
+
+insert into patient values(1,'vinay','Banglore','Male');
+insert into patient values(2,'prajwal','Banglore','Male');
+insert into patient values(3,'vidya','Manglore','Female');
+insert into patient values(4,'dhanu','Mumbai','Female');
+
+insert into doctor values('Dr.Ram',21,'oncology');
+insert into doctor values('Dr.Roy',22,'Haematology');
+insert into doctor values('Dr.Lakshmi',23,'Neurology');
+insert into doctor values('Dr.Sita',24,'ENT');
+
+insert into lab_report values(1,100,585,24,'2023-11-05');
+insert into lab_report values(2,101,585,21,'2023-11-13');
+insert into lab_report values(3,102,585,23,'2023-10-23');
+insert into lab_report values(4,103,585,22,'2023-10-15');
+
+insert into inpatient values(50,100,1,'2023-04-23','2023-08-16');
+insert into inpatient values(51,101,2,'2023-05-15','2023-07-13');
+insert into inpatient values(52,102,3,'2023-02-23','2023-10-06');
+insert into inpatient values(53,103,4,'2023-08-23','2023-12-21');
+
+insert into outpatient values(100,1,'2023-07-23');
+insert into outpatient values(101,2,'2023-05-30');
+insert into outpatient values(103,3,'2023-10-31');
+insert into outpatient values(102,4,'2023-11-01');
+
+insert into rooms values(50,'single','available');
+insert into rooms values(51,'sharing','not_available');
+insert into rooms values(52,'sharing','available');
+insert into rooms values(53,'single','available');
+
+insert into bills values('1bgs01',1,10,789,50);
+insert into bills values('1bgs02',2,23,550,50);
+insert into bills values('1bgs03',3,5,400,50);
+insert into bills values('1bgs04',4,24,1560,50);
+
+
+
